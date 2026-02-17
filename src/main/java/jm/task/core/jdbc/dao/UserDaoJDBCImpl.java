f17d8c13b0bed040230374c47fc87023fcfbab87
LOGPVR <214538967+LOGPVR@users.noreply.github.com>
2026-02-18 02:45:48 +0600

    the createUsersTable method is implemented, tested in main

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,7 +1,11 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
@@ -10,7 +14,19 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void createUsersTable() {
+        String sql = "CREATE TABLE IF NOT EXISTS students(" +
+                "id BIGINT PRIMARY KEY AUTO_INCREMENT," +
+                "name VARCHAR(35)," +
+                "last_name VARCHAR(65)," +
+                "age TINYINT UNSIGNED)";
 
+        try(Connection connection = Util.getConnect()) {
+            Statement statement = connection.createStatement();
+            statement.execute(sql);
+            System.out.println("The students table was created successfully!");
+        } catch (SQLException e) {
+            System.out.println("Error creating table students:\n" + e.getMessage());
+        }
     }
 
     public void dropUsersTable() {
--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -2,6 +2,7 @@ package jm.task.core.jdbc.service;
 
 import jm.task.core.jdbc.model.User;
 
+import javax.swing.*;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
