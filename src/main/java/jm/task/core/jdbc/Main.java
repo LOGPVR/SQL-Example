da608b8ae837c84ed588323667eeca4ecd68391b
LOGPVR <214538967+LOGPVR@users.noreply.github.com>
2026-02-18 19:49:16 +0600

    All dao and service methods are implemented, tested in main, all UserServiceTest tests are passed

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,11 +1,32 @@
 package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserServiceImpl;
 import jm.task.core.jdbc.util.Util;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
-        userDaoJDBC.createUsersTable();
+        UserServiceImpl userService = new UserServiceImpl();
+
+        userService.createUsersTable();
+
+        userService.saveUser("Ivan", "Ivanov", (byte) 18);
+        userService.saveUser("Sidor", "Sidorov", (byte) 45);
+        userService.saveUser("Pavel", "Pavlov", (byte) 33);
+        userService.saveUser("Vasily", "Vasiliev", (byte) 26);
+
+        List<User> userList = userService.getAllUsers();
+        System.out.println(userList);
+
+        userService.removeUserById(2);
+        List<User> userList1 = userService.getAllUsers();
+        System.out.println("После метода removeUserById:\n" + userList1);
+
+        userService.cleanUsersTable();
+
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,25 +1,16 @@
 package jm.task.core.jdbc.util;
 
-import com.mysql.cj.callback.UsernameCallback;
-
-import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Util {
-    private final static String URL = "jdbc:mysql://localhost:3306/dbstudents";
+    private final static String URL = "jdbc:mysql://localhost:3306/dbusers";
     private final static String USERNAME = "root";
     private final static String PASSWORD = "0000";
 
-    public static Connection getConnect() {
-        Connection connection = null;
-        try {
-            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-            System.out.println("Connection access!");
-        } catch (SQLException e) {
-            System.out.println("Connection error" + e);;
-        }
+    public static Connection getConnect() throws SQLException {
+        Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
         return connection;
     }
 }
