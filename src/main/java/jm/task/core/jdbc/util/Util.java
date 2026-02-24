b538592f1c3718efa439681ffda377318a97ba67
LOGPVR <214538967+LOGPVR@users.noreply.github.com>
2026-02-24 19:25:39 +0600

    Unused code removed

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -3,19 +3,12 @@ package jm.task.core.jdbc.util;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 
 public class Util {
     private final static String URL = "jdbc:mysql://localhost:3306/dbusers";
     private final static String USERNAME = "root";
     private final static String PASSWORD = "0000";
 
-    public static Connection getConnect() throws SQLException {
-        Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-        return connection;
-    }
 
     private static SessionFactory sessionFactory = buildSessionFactory();
 
